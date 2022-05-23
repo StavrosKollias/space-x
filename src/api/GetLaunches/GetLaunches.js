c453f6e31b878cc5b53f0cd008f50aa8e9e39e1f
Stavros Kollias <stavros.kollias@co-operativebank.co.uk>
2022-05-23 11:42:26 +0100

    cleaning code

--- src/api/GetLaunches/GetLaunches.js
+++ src/api/GetLaunches/GetLaunches.js
@@ -1,19 +1,4 @@
 // Change below function to API call
-// import axios from "axios";
-
-// export const GetLaunchesAPI = (url) => {
-//     const onSuccess = (response) => {
-//         return response;
-//     };
-
-//     const onError = (err) => {
-//         return { error: err.message };
-//     };
-
-//     return axios.get(url).then(onSuccess).catch(onError);
-// };
-
-// Change below function to API call
 export const GetLaunchesAPI = async (url) => {
     return fetch(url)
         .then((res) => {
